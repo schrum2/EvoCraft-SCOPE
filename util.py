3d79e664a03ca8dd050ee057715b1eba9650c1a2
Alejandro Medina <medina4@southwestern.edu>
2022-05-19 10:47:28 -0500

    #11 Made new modules
    Made the following modules: minecraft_structures, neat_stagnation, and util. Each now contain functions or classes that were originally causing the evolve_CPPN module to look more messy. The functions still seem to work.

---
+++ util.py
@@ -0,0 +1,34 @@
+"""
+Utility functions used will be kept in this module.
+"""
+import math
+
+def argmax(l):
+    """
+    Finds the maximum value in a list of values
+    :param l a list of numeric elements
+    :return index of first maximal element
+    """
+    f = lambda i: l[i]
+    return max(range(len(l)), key=f)
+
+def distance(v, u):
+    """
+    Euclidean distance between two vectors of the same length.
+    :param u a vectors
+    :param v other vector
+    :return distance between vectors
+    """
+    d = 0
+    for i in range(len(u)):
+        d += (u[i] - v[i])**2
+    return math.sqrt(d)
+
+def scale_and_center(index, top):
+    """
+    This scales the block index to the range [-1,1]
+    
+    :param index: index of block along a given dimension
+    :param top: number of blocks along the given dimension
+    """
+    return -1.0 + 2.0 * index / (top - 1)
