8823209114d3e21ce9e75dcc9414fdaf304ba8e0
richeyme <richey2@southwestern.edu>
2022-05-23 11:04:24 -0500

    #23 Created new method that detects a block
    The method returns a block if there is one and none if there is not. In the query cppn for shape method, the previously mentioned method is called and used to determine whether there is a block that can be appended to shape. I think that the amount of parameters in the block detection method can be reduced but this is what I have right now that works

--- cppn_generation.py
+++ cppn_generation.py
@@ -31,7 +31,7 @@ def query_cppn_for_shape(genome, config, corner, position_information, args, blo
         else:
             block_options = genome.block_list
 
-        net = neat.nn.FeedForwardNetwork.create(genome, config) # Create CPPN out of genome
+        
         shape = []
         for xi in range(position_information["xrange"]):
             x = util.scale_and_center(xi,position_information["xrange"])
@@ -39,23 +39,8 @@ def query_cppn_for_shape(genome, config, corner, position_information, args, blo
                 y = util.scale_and_center(yi,position_information["yrange"])
                 for zi in range(position_information["zrange"]):
                     z = util.scale_and_center(zi,position_information["zrange"])
-                    # math.sqrt(2) is the usual scaling for radial distances in CPPNs
-                    center_dist = util.distance((x,y,z),(0,0,0))
-                    output = net.activate([x, y, z, center_dist * math.sqrt(2), 1.0])
-                    
-                    # First output determines whether there is a block at all.
-                    # If there is a block, argmax determines the max value and places the specified block 
-                    # from the list of possible blocks
-                    presence_threshold = args.PRESENCE_THRESHOLD 
-                    
-                    if args.DISTANCE_PRESENCE_THRESHOLD:
-                        presence_threshold = args.DISTANCE_PRESENCE_MULTIPLIER * center_dist                            
-
-                    # Only generate non-air blocks
-                    if output[0] >= presence_threshold: 
-                        output_val = util.argmax(output[1:])
-                        assert (output_val >= 0 and output_val < len(block_options)),"{} out of bounds: {}".format(output_val,block_options)
-                        block = Block(position=Point(x=corner[0]+xi, y=corner[1]+yi, z=corner[2]+zi), type=block_options[output_val], orientation=NORTH)
+                    block = is_there_a_block(genome, config, corner, args, block_options, x, y, z, xi, yi, zi)
+                    if block is not None:
                         shape.append(block)
         
         if(len(shape) == 0):
@@ -66,3 +51,30 @@ def query_cppn_for_shape(genome, config, corner, position_information, args, blo
             print("Genome at corner {} generated {} blocks of these types: {}".format(corner,len(shape),set(map(lambda x: BlockType.keys()[x.type], shape))))
 
         return shape
+
+def is_there_a_block(genome, config, corner, args, block_options, x, y, z, xi, yi, zi):
+
+    net = neat.nn.FeedForwardNetwork.create(genome, config) # Create CPPN out of genome
+
+    # math.sqrt(2) is the usual scaling for radial distances in CPPNs
+    center_dist = util.distance((x,y,z),(0,0,0))
+    output = net.activate([x, y, z, center_dist * math.sqrt(2), 1.0])
+                    
+    # First output determines whether there is a block at all.
+    # If there is a block, argmax determines the max value and places the specified block 
+    # from the list of possible blocks
+    presence_threshold = args.PRESENCE_THRESHOLD 
+                    
+    if args.DISTANCE_PRESENCE_THRESHOLD:
+        presence_threshold = args.DISTANCE_PRESENCE_MULTIPLIER * center_dist                            
+
+    # Only generate non-air blocks
+    if output[0] >= presence_threshold: 
+        output_val = util.argmax(output[1:])
+        assert (output_val >= 0 and output_val < len(block_options)),"{} out of bounds: {}".format(output_val,block_options)
+        block = Block(position=Point(x=corner[0]+xi, y=corner[1]+yi, z=corner[2]+zi), type=block_options[output_val], orientation=NORTH)
+        return block
+    else:
+        return None
+
+#def query_cppn_for_snake_shape():
