f5fc23d317a4e4245be0087d5446de91449e0557
Alejandro Medina <medina4@southwestern.edu>
2022-05-24 16:14:54 -0500

    #43 Added if condition for saving fitness
    I realized the plotting has to happen for fitness based evolution so I placed the csv saving inside an if condition that only hits when INTERACTIVE_EVOLUTION is false.

--- evolution.py
+++ evolution.py
@@ -83,8 +83,11 @@ def run(args):
             pop.run(mc.eval_fitness, generations)
 
     finally:
-        stats.save()
-        stats.save_genome_fitness(with_cross_validation=False)
+        # only save to csv for fitness based evolution
+        if not args.INTERACTIVE_EVOLUTION:
+            stats.save()
+            # cross_validation has to be false, true produces an error, also the git thing said
+            stats.save_genome_fitness(with_cross_validation=False)
     
         # Clear and reset lots of extra space on exit/crash unless KEEP_WORLD_ON_EXIT is true. Population size doubled to clear more space
         if not args.KEEP_WORLD_ON_EXIT:
