aa420bc10092b327c0141ea90ebdad5624290454
richeyme <richey2@southwestern.edu>
2022-05-24 09:27:32 -0500

    #40 Created command line parameter
    The command line parameter --CONFINE_SNAKES was made and set to true as the default

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ def main(argv):
                         help='Changes the CPPN to generate snake-like structures.')
     parser.add_argument('--MAX_SNAKE_LENGTH', type=int, default=100, metavar='',
                         help='The maximum length a snake-like structure can be when EVOLVE_SNAKE is true.')
+    parser.add_argument('--CONFINE_SNAKES', type=boolean_string, default=True, metavar='',
+                        help='Confines the snake generations so that they do not cross with other snakes.')
     parser.add_argument('--CONTINUATION_THRESHOLD', type=float, default=0.5, metavar='',
                         help='The maximum length a snake-like structure can be when EVOLVE_SNAKE is true.')
     parser.add_argument('--INTERACTIVE_EVOLUTION', type=boolean_string, default=True, metavar='',
