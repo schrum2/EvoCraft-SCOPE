ddc70092a09fd2a97691a7e814b4d05e4ef5f558
richeyme <richey2@southwestern.edu>
2022-05-26 16:45:27 -0500

    #42 Tested out the redirect snakes up
    I don't think it's working as it should because the snakes are going in all directions instead of being confined in the x and z direction

--- cppn_generation.py
+++ cppn_generation.py
@@ -139,8 +139,8 @@ def generate_block(net, position_information, corner, args, block_options, scale
             if check_out_of_bounds(relative_position, direction, position_information):
                 stop = True
     
-        if args.CONFINE_SNAKES and args.REDIRECT_CONFINED_SNAKES_UP
-            for i in range(NUM_DIRECTIONS):
+        if args.CONFINE_SNAKES and args.REDIRECT_CONFINED_SNAKES_UP:
+            for i in range(NUM_DIRECTIONS - 1):
                 possible_direction = next_direction(i)
                 if check_out_of_bounds(relative_position, possible_direction, position_information, True):
                     direction_preferences[i] = float('-inf')
