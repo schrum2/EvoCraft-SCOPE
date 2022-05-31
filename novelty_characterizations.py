5a7700c0cfc727b5814b7fe4bade4744822298cb
Mark Mueller <muellerm@southwestern.edu>
2022-05-31 10:18:21 -0500

    #35 fixed how the blocks are added to characterization
    fixed the for loop so now the for loop actually works for presence_characterization

--- novelty_characterizations.py
+++ novelty_characterizations.py
@@ -7,15 +7,21 @@ def presence_characterization(client, position_information, corner, args):
     endz= corner[2] + position_information["zrange"]
 
     # read all the blocks at once as one big block
-    blocks = client.readCube(Cube(
+    block_collection = client.readCube(Cube(
         min=Point(x=corner[0], y=corner[1], z=corner[2]),
         max=Point(x=endx, y=endy, z=endz)
     ))
 
-    block_collection = [] 
-    for i in range(len(blocks)):
-        if(blocks[i].blocks.value==AIR):
-            block_collection.append(0)
+    block_character = [] 
+    for block in block_collection.blocks:
+        if block.type == AIR:
+            block_character.append(0)
         else:
-            block_collection.append(1)
+            block_character.append(1)
+    # for i in range(len(blocks)):
+        
+    #     if(blocks[i].blocks.value==AIR):
+    #         block_collection.append(0)
+    #     else:
+    #         block_collection.append(1)
     return block_collection
