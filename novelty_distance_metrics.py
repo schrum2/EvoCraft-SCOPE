9a9e061df213545ab943b82a8441215011a1a75e
Mark Mueller <muellerm@southwestern.edu>
2022-06-01 11:35:35 -0500

    #65 Added hamming distance
    Hamming distance is now an option in novelty_distance_metrics, complete with comments

--- novelty_distance_metrics.py
+++ novelty_distance_metrics.py
@@ -1,6 +1,31 @@
 from minecraft_pb2 import *
 import numpy as np
 
+
 def euclidean_distance(arr1,arr2):
+    """
+    Calculates the euclidean distance between two arrays
+    
+    Parameters:
+    arr1(numpy array): Array containg the information of one shape
+    arr2(numpy array): Array containg the information of another shape
+
+    Return:
+    edist (float): The distance calculated between the two arrays
+    """
     edist = np.linalg.norm(arr1.ravel() - arr2.ravel())
-    return edist
+    return float(edist)
+
+def hamming_distance(arr1,arr2):
+    """
+    Calculates the hamming distance between two arrays
+    
+    Parameters:
+    arr1(numpy array): Array containg the information of one shape
+    arr2(numpy array): Array containg the information of another shape
+
+    Return:
+    hdist (float): The distance calculated between the two arrays
+    """
+    hdist = sum(h1 != h2 for h1, h2 in zip(arr1, arr2))
+    return float(hdist)
