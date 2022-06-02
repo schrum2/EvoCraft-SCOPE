19b7a8a56e3de154144c14efd4a8495e2d3814fc
Mark Mueller <muellerm@southwestern.edu>
2022-06-02 08:34:18 -0500

    #70 Started making tests for euclidean
    started making tests, however, thought it would be best for the arrays to be global. commiting this first to work on that

--- test_novelty_distance_metric.py
+++ test_novelty_distance_metric.py
@@ -3,4 +3,14 @@ import pytest
 import numpy as np
 
 def test_euclidean_distance():
-    list1 = []
+    list1 = [1,2,3,4,5]
+    list2 = [1,2,3,4,5]
+
+    arr1 = np.array(list1)
+    arr2 = np.array(list2)
+
+    zeroes = np.zeros(50)
+    ones = np.ones(50)
+
+    assert ndm.euclidean_distance(arr1,arr2) == 0
+    assert ndm.euclidean_distance(ones,zeroes) == np.sqrt(50)
