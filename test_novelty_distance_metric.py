bf736deb7a6e8a6342292d7a8eab2a7268de10a4
Mark Mueller <muellerm@southwestern.edu>
2022-06-02 09:46:17 -0500

    #70 ALL hamming distance tests
    all hamming distance tests added, at least for now

--- test_novelty_distance_metric.py
+++ test_novelty_distance_metric.py
@@ -2,10 +2,12 @@ import novelty_distance_metrics as ndm
 import pytest
 import numpy as np
 
-# Global vars
+# Global variables, stores in a dictionary
 def pytest_namespace():
     return {'arr1': np.array([1,2,3,4,5]),'arr2': np.array([1,2,3,4,5]),'arr3': np.array([0,0,0,0,0]),'arr4': np.array([5,4,3,2,1]),
     'long_arr1': np.array([1,2,3,4,5,6,7,8,9,10]),'long_arr2': np.array([1,80,0,9,8,8,2,3,0,0]),'long_arr3': np.array([2,34,8,5,-2,30,-123,7,23,4]),
+    'hamming_arr1': np.array([8,6,7,5,3,0,9,1,8,0,0,9,8,8,2,3,0,0]),'hamming_arr2': np.array([8,6,7,5,3,0,9,12,-90,12,564,-1000,5,21,564,9,10,0]),
+    'hamming_arr3': np.array([5,5,5,5,5,9,5,5,8,5,5,5,5,5,3,2,5,5]),'hamming_arr4': np.array([5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]),
     'point1': np.array([0,0]),'point2': np.array([2,2]),'point3': np.array([5,-1]),'point4': np.array([-3,6]),'zeroes':np.zeros(50),'ones':np.ones(50)}
 
 def test_euclidean_distance():
@@ -32,3 +34,36 @@ def test_euclidean_distance():
     assert ndm.euclidean_distance(pytest_namespace()['ones'],pytest_namespace()['zeroes']) == np.sqrt(50)
     assert ndm.euclidean_distance(pytest_namespace()['ones'],pytest_namespace()['ones']) == 0
     assert ndm.euclidean_distance((pytest_namespace()['ones']+3),pytest_namespace()['zeroes']) == np.sqrt(800)
+
+def test_hamming_distance():
+    assert ndm.hamming_distance(pytest_namespace()['arr1'],pytest_namespace()['arr2']) == 0
+    assert ndm.hamming_distance(pytest_namespace()['arr1'],pytest_namespace()['arr3']) == 5
+    assert ndm.hamming_distance(pytest_namespace()['arr1'],pytest_namespace()['arr4']) == 4
+    assert ndm.hamming_distance(pytest_namespace()['arr2'],pytest_namespace()['arr3']) == 5
+    assert ndm.hamming_distance(pytest_namespace()['arr2'],pytest_namespace()['arr4']) == 4
+    assert ndm.hamming_distance(pytest_namespace()['arr3'],pytest_namespace()['arr4']) == 5
+
+    assert ndm.hamming_distance(pytest_namespace()['point1'],pytest_namespace()['point2']) == 2
+    assert ndm.hamming_distance(pytest_namespace()['point1'],pytest_namespace()['point3']) == 2
+    assert ndm.hamming_distance(pytest_namespace()['point1'],pytest_namespace()['point4']) == 2
+    assert ndm.hamming_distance(pytest_namespace()['point2'],pytest_namespace()['point3']) == 2
+    assert ndm.hamming_distance(pytest_namespace()['point2'],pytest_namespace()['point4']) == 2
+    assert ndm.hamming_distance(pytest_namespace()['point3'],pytest_namespace()['point4']) == 2
+    assert ndm.hamming_distance(pytest_namespace()['point3'],pytest_namespace()['point3']) == 0
+
+    assert ndm.hamming_distance(pytest_namespace()['long_arr1'],pytest_namespace()['long_arr1']) == 0
+    assert ndm.hamming_distance(pytest_namespace()['long_arr1'],pytest_namespace()['long_arr2']) == 9
+    assert ndm.hamming_distance(pytest_namespace()['long_arr1'],pytest_namespace()['long_arr3']) == 10
+    assert ndm.hamming_distance(pytest_namespace()['long_arr2'],pytest_namespace()['long_arr3']) == 10
+
+    assert ndm.hamming_distance(pytest_namespace()['hamming_arr1'],pytest_namespace()['hamming_arr2']) == 10
+    assert ndm.hamming_distance(pytest_namespace()['hamming_arr1'],pytest_namespace()['hamming_arr3']) == 16
+    assert ndm.hamming_distance(pytest_namespace()['hamming_arr1'],pytest_namespace()['hamming_arr4']) == 17
+    assert ndm.hamming_distance(pytest_namespace()['hamming_arr2'],pytest_namespace()['hamming_arr3']) == 16
+    assert ndm.hamming_distance(pytest_namespace()['hamming_arr2'],pytest_namespace()['hamming_arr4']) == 16
+    assert ndm.hamming_distance(pytest_namespace()['hamming_arr3'],pytest_namespace()['hamming_arr4']) == 4
+
+    assert ndm.hamming_distance(pytest_namespace()['ones'],pytest_namespace()['zeroes']) == 50
+    assert ndm.hamming_distance(pytest_namespace()['ones'],pytest_namespace()['ones']) == 0
+    assert ndm.hamming_distance((pytest_namespace()['ones']+3),pytest_namespace()['zeroes']) == 50
+    assert ndm.hamming_distance((pytest_namespace()['ones']-1),pytest_namespace()['zeroes']) == 0
