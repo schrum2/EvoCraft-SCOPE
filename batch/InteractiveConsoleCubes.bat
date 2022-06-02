bffe2d5ed19101f96365c8cf9011478a6877dac0
Alejandro Medina <medina4@southwestern.edu>
2022-06-02 14:59:11 -0500

    Updated batch files to support new command line parameter EVOLVE_FITNESS
    After I merged today, I noticed I needed to specify the command line parameter EVOLVE_FITNESS. Seems it was added with the new novelty things. I added this command line parameter to the existing batch files.

--- batch/InteractiveConsoleCubes.bat
+++ batch/InteractiveConsoleCubes.bat
@@ -1,3 +1,3 @@
 REM Usage ./InteractiveConsoleCubes.bat <seed>
 cd..
-python main.py --BASE_DIR=console --EXPERIMENT_PREFIX=cubes --RANDOM_SEED=%1 --POPULATION_SIZE=10 --USE_ELITISM=True
+python main.py --BASE_DIR=console --EXPERIMENT_PREFIX=cubes --RANDOM_SEED=%1 --POPULATION_SIZE=10 --USE_ELITISM=True --INTERACTIVE_EVOLUTION=True
