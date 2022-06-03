54e3aafa392a0fd8c5d4a8fd613083a995071300
Mark Mueller <muellerm@southwestern.edu>
2022-06-03 09:57:04 -0500

    #71 All custom hamming batch files done
    finished all batches based on each possible pairing of characterisation and distance metric. Will add a few more

---
+++ batch/NoveltyCustomHammingDistanceCompositionCharacteristic.bat
@@ -0,0 +1,3 @@
+REM Usage ./NoveltyCustomHammingDistanceCompositionCharacteristic.bat <seed>
+cd..
+python main.py --BASE_DIR=novelty --EXPERIMENT_PREFIX=basic --RANDOM_SEED=%1 --POPULATION_SIZE=10 --SAVE_NOVELTY=True --NOVELTY_RANDOM_SCORE=0.05 --EVOLVE_NOVELTY=True --POTENTIAL_BLOCK_SET=machine --INTERACTIVE_EVOLUTION=False --NOVELTY_DISTANCE=custom_hamming_distance --NOVELTY_CHARACTER=composition_characterization --SAVE_PARAMETERS=True
