d444cf945eab08c523fa3c77774519c8d9ac3156
jschoolprogrammingg <garcia50@southwestern.edu>
2026-05-22 16:37:53 -0500

    Create missile.py
    Here I used the example from EvoCraft.py to create a new python file that makes an red stone machine that propels forward by itself and explodes TNT on impact when making contact with a red stone wall. Though I haven't yet reached creating a red stone machine that explodes TNT on impact with any block, this is a step forward towards reaching that point. The following YouTube video was used to create the successful TNT red stone wall exploding machine: https://www.youtube.com/watch?v=ndfmR8lsTnc. The following YouTube videos were also used in creating the non-successful red stone wall machines: https://www.youtube.com/watch?v=Z4VuRYqlv5Q , https://www.youtube.com/watch?v=IgYgvoyAQrk , https://www.youtube.com/watch?v=7xJe6oxDAzU (involves use of honey block which is unavailable in 1.12.2)

---
+++ missile.py
@@ -0,0 +1,47 @@
+import grpc
+
+import minecraft_pb2_grpc
+from minecraft_pb2 import *
+
+channel = grpc.insecure_channel('localhost:5001')
+client = minecraft_pb2_grpc.MinecraftServiceStub(channel)
+
+client.fillCube(FillCubeRequest(  # Clear a 20x10x20 working area
+    cube=Cube(
+        min=Point(x=-20, y=3, z=-150),
+        max=Point(x=20, y=20, z=20)
+    ),
+    type=AIR
+))
+
+client.spawnBlocks(Blocks(blocks=[
+
+    # CORE PISTON LINE
+    Block(position=Point(x=0, y=5, z=0), type=STICKY_PISTON, orientation=WEST),
+    Block(position=Point(x=1, y=5, z=0), type=PISTON, orientation=EAST),
+    Block(position=Point(x=-1, y=5, z=0), type=SLIME, orientation=NORTH),
+    Block(position=Point(x=-2, y=5, z=0), type=PISTON, orientation=EAST),
+
+    # SLIME EXTENSION
+    Block(position=Point(x=2, y=5, z=0), type=SLIME, orientation=NORTH),
+    Block(position=Point(x=3, y=5, z=0), type=SLIME, orientation=NORTH),
+
+    # REDSTONE TRIGGERS
+    Block(position=Point(x=-1, y=6, z=0), type=REDSTONE_BLOCK, orientation=NORTH),
+    Block(position=Point(x=2, y=6, z=0), type=REDSTONE_BLOCK, orientation=NORTH),
+
+    # TNT PAYLOAD
+    Block(position=Point(x=3, y=4, z=0), type=TNT, orientation=NORTH),
+    Block(position=Point(x=3, y=5, z=-1), type=TNT, orientation=NORTH),
+    Block(position=Point(x=3, y=5, z=1), type=TNT, orientation=NORTH),
+    Block(position=Point(x=4, y=5, z=0), type=TNT, orientation=NORTH),
+
+    Block(position=Point(x=-2, y=4, z=0), type=SLIME, orientation=WEST),
+
+]))
+
+
+blocks = client.readCube(Cube(
+    min=Point(x=1, y=5, z=-4),
+    max=Point(x=1, y=6, z=1)
+))
