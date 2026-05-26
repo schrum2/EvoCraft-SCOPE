5c6695158edcc6f1107e637a8c225d6b219e8f4b
jschoolprogrammingg <garcia50@southwestern.edu>
2026-05-26 13:55:41 -0500

    Created successful missile machine

--- missile.py
+++ missile.py
@@ -1,4 +1,5 @@
 import grpc
+import time
 
 import minecraft_pb2_grpc
 from minecraft_pb2 import *
@@ -8,37 +9,77 @@ client = minecraft_pb2_grpc.MinecraftServiceStub(channel)
 
 client.fillCube(FillCubeRequest(  # Clear a 20x10x20 working area
     cube=Cube(
-        min=Point(x=-20, y=3, z=-150),
-        max=Point(x=20, y=20, z=20)
+        min=Point(x=-5, y=5, z=5),
+        max=Point(x=7, y=15, z=-3)
     ),
     type=AIR
 ))
 
 client.spawnBlocks(Blocks(blocks=[
-    Block(position=Point(x=0, y=6, z=0), type=TNT, orientation=WEST),
-    Block(position=Point(x=1, y=6, z=0), type=REDSTONE_LAMP, orientation=WEST),
-    Block(position=Point(x=2, y=6, z=0), type=OBSERVER, orientation=SOUTH),
-    Block(position=Point(x=3, y=6, z=0), type=SLIME, orientation=WEST),
-    Block(position=Point(x=4, y=6, z=0), type=SLIME, orientation=WEST),
-    Block(position=Point(x=0, y=5, z=0), type=SLIME, orientation=WEST),
-    Block(position=Point(x=1, y=5, z=0), type=SLIME, orientation=WEST),
-    Block(position=Point(x=2, y=5, z=0), type=STICKY_PISTON, orientation=EAST),
-    Block(position=Point(x=4, y=5, z=0), type=SLIME, orientation=WEST),
+    # Origin slime block
+    Block(position=Point(x=0, y=10, z=0), type=SLIME, orientation=SOUTH),
 
-    Block(position=Point(x=0, y=6, z=1), type=SLIME, orientation=WEST),
-    Block(position=Point(x=1, y=6, z=1), type=SLIME, orientation=WEST),
-    Block(position=Point(x=2, y=6, z=1), type=STICKY_PISTON, orientation=WEST),
-    Block(position=Point(x=3, y=6, z=1), type=STICKY_PISTON, orientation=UP),
-    Block(position=Point(x=4, y=6, z=1), type=SLIME, orientation=WEST),
-    Block(position=Point(x=0, y=5, z=1), type=SLIME, orientation=WEST),
-    Block(position=Point(x=2, y=5, z=1), type=OBSERVER, orientation=UP),
-    Block(position=Point(x=3, y=5, z=1), type=SLIME, orientation=WEST),
-    Block(position=Point(x=4, y=5, z=1), type=STICKY_PISTON, orientation=EAST),
-    Block(position=Point(x=4, y=7, z=1), type=FIRE, orientation=EAST),
+    # Front-facing observers left/right/up/down of origin slime
+    Block(position=Point(x=-1, y=10, z=0), type=OBSERVER, orientation=NORTH),  # left
+    Block(position=Point(x=1,  y=10, z=0), type=OBSERVER, orientation=NORTH),  # right
+    Block(position=Point(x=0,  y=11, z=0), type=OBSERVER, orientation=NORTH),  # up
+    Block(position=Point(x=0,  y=9,  z=0), type=OBSERVER, orientation=NORTH),  # down
+
+    # Slime block in front of origin slime
+    Block(position=Point(x=0, y=10, z=1), type=SLIME, orientation=SOUTH),
+
+    # Front-facing pistons left/right/up/down of origin slime
+    Block(position=Point(x=-1, y=10, z=1), type=PISTON, orientation=SOUTH),  # left
+    Block(position=Point(x=1,  y=10, z=1), type=PISTON, orientation=SOUTH),  # right
+    Block(position=Point(x=0,  y=11, z=1), type=PISTON, orientation=SOUTH),  # up
+    Block(position=Point(x=0,  y=9,  z=1), type=PISTON, orientation=SOUTH),  # down
+
+    # Air, slime, TNT in front of LEFT piston
+    Block(position=Point(x=-1, y=10, z=2), type=AIR, orientation=SOUTH),
+    Block(position=Point(x=-1, y=10, z=3), type=SLIME, orientation=SOUTH),
+    Block(position=Point(x=-1, y=10, z=4), type=TNT, orientation=SOUTH),
+
+    # Back-facing sticky piston behind observer
+    Block(position=Point(x=0, y=10, z=3), type=STICKY_PISTON, orientation=NORTH),
+
+    # TNT to the left of sticky piston
+    Block(position=Point(x=1, y=10, z=3), type=TNT, orientation=SOUTH),
+
+    # TNT to the left of observer
+    Block(position=Point(x=1, y=10, z=4), type=TNT, orientation=SOUTH),
+
+    # TNT above sticky piston
+    Block(position=Point(x=0, y=11, z=3), type=TNT, orientation=SOUTH),
+
+    # TNT above observer
+    Block(position=Point(x=0, y=11, z=4), type=TNT, orientation=SOUTH),
+
+    # TNT below sticky piston
+    Block(position=Point(x=0, y=9, z=3), type=TNT, orientation=SOUTH),
+
+    # TNT below observer
+    Block(position=Point(x=0, y=9, z=4), type=TNT, orientation=SOUTH),
+
+    Block(position=Point(x=-1, y=10, z=5), type=TNT, orientation=SOUTH),
+    Block(position=Point(x=1, y=10, z=5), type=TNT, orientation=SOUTH),
+    Block(position=Point(x=0, y=11, z=5), type=TNT, orientation=SOUTH),
+    Block(position=Point(x=0, y=9, z=5), type=TNT, orientation=SOUTH),
+
+    Block(position=Point(x=-1, y=10, z=6), type=TNT, orientation=SOUTH),
+    Block(position=Point(x=1, y=10, z=6), type=TNT, orientation=SOUTH),
+    Block(position=Point(x=0, y=11, z=6), type=TNT, orientation=SOUTH),
+    Block(position=Point(x=0, y=9, z=6), type=TNT, orientation=SOUTH),
+
+    Block(position=Point(x=0, y=10, z=7), type=REDSTONE_BLOCK, orientation=SOUTH),
+    Block(position=Point(x=0, y=10, z=6), type=QUARTZ_BLOCK, orientation=SOUTH),
+    # Front-facing observer to the left of TNT
+    Block(position=Point(x=0, y=10, z=4), type=OBSERVER, orientation=SOUTH),
 ]))
-
-
 blocks = client.readCube(Cube(
     min=Point(x=1, y=5, z=-4),
     max=Point(x=1, y=6, z=1)
 ))
+# time.sleep(0.1)
+client.spawnBlocks(Blocks(blocks=[
+    Block(position=Point(x=0, y=10, z=5), type=TNT, orientation=SOUTH),
+]))
